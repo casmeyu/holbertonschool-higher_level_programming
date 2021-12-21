b048b7a5f40d1f1dcbf73fbb56cb9390f024fb4e
ojo-perezoso <bymartin24@gmail.com>
2021-12-21 07:43:58 -0800

    Added .pformat to alphabets

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(26):
     if (chr((ord('a') + i)) != 'q' and chr((ord('a') + i)) != 'e'):
-        print(chr(ord('a') + i), end='')
+        print("{}".format(chr(ord('a') + i)), end='')
