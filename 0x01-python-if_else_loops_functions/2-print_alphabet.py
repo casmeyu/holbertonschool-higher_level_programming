4b79e4e20e333b971a21f711cb061afff80e562c
ojo-perezoso <bymartin24@gmail.com>
2021-12-21 07:06:31 -0800

    fix try 1 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 char = ord('a') #ascii
-alphabet = ""
 while (char <= ord('z')):
-    alphabet += chr(char)
-    char += 1 
-print(alphabet, end = '')
+    print(chr(char), end = '')
+    char += 1
