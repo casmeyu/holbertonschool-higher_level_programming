24683bc650f8995143d4334170a6113a52b51390
ojo-perezoso <bymartin24@gmail.com>
2021-12-20 16:47:45 -0800

    fix 5 lines of code

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-auxstr = str[39:67]
-auxstr += str[7:10]
-auxstr += str[107:112]
-auxstr += str[0:6]
-str = auxstr
+str = str[39:67] + str[7:10] + str[107:112] + str[0:6]
 print(str)
