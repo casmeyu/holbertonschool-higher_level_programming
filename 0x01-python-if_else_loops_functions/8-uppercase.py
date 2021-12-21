bb7fa19db8e63588552a3786fc3cb7db99585291
ojo-perezoso <bymartin24@gmail.com>
2021-12-21 14:27:29 -0800

    Added 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str = list(str)
+    for i in range(len(str)):
+        if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
+            str[i] = chr(ord(str[i]) - 32)
+    print("{}".format(''.join(str)))
