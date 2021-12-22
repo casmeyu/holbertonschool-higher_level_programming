5e5410d0019c82118d195bf5db6e11051cce4188
ojo-perezoso <bymartin24@gmail.com>
2021-12-21 18:06:33 -0800

    fix try for 1 checker of 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ digit = abs(number) % 10
 answer = "Last digit of {:d} is {:d}".format(number, digit)
 if (digit > 5):
     answer += " and is greater than 5"
-elif (digit == 0):
-    answer += " and is 0"
 elif (digit < 6 and digit != 0):
     answer += " and is less than 6 and not 0"
+else:
+    answer += " and is 0"
 
 print("{}".format(answer))
