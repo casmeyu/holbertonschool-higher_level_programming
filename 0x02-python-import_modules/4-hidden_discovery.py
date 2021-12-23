ef2597c349f9f1f0d054b0979a514bea453b14fd
ojo-perezoso <bymartin24@gmail.com>
2021-12-23 09:22:51 -0800

    fix try 3 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import sys, hidden_4
 if __name__ == "__main__":
-    items = dir(hidden_4)
-    items.sort()
-    for item in items:
-        if (item[0] != "_" and item[1] != "_"):
+    for item in dir(hidden_4):
+        if (item[:2]!= "__"):
             print(item)
