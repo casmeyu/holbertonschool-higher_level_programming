1a524974fc5e210dbd134a936dae7451fe420994
ojo-perezoso <bymartin24@gmail.com>
2021-12-23 07:31:05 -0800

    2-args.py only executable if __main__

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 import sys
-argc = (len(sys.argv))
 
-if (argc == 1):
-    print("0 arguments.")
-    exit()
+if __name__ = "__main__":
+    argc = (len(sys.argv))
 
-if (argc == 2):
-    print("1 argument:")
-else:
-    print("{:d} arguments:".format(argc - 1))
+    if (argc == 1):
+        print("0 arguments.")
+        exit()
 
-for i in range(1, argc):
-    print("{:d}: {}".format(i, sys.argv[i]))
+    if (argc == 2):
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(argc - 1))
+
+    for i in range(1, argc):
+        print("{:d}: {}".format(i, sys.argv[i]))
