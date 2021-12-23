2d81baef924f002f883c4814d80687b06f1c23af
ojo-perezoso <bymartin24@gmail.com>
2021-12-23 09:14:50 -0800

    4-hidden_discovery executable if __main__

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys, hidden_4
-for item in dir(hidden_4):
-    if (item[0] != "_" or item[1] != "_"):
-        print(item)
+if __name__ = "__main__":
+    for item in dir(hidden_4):
+        if (item[0] != "_" or item[1] != "_"):
+            print(item)
