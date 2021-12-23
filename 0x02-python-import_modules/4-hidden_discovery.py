8f382b5ea99950e6582c3dac5bf58abccaadfae4
ojo-perezoso <bymartin24@gmail.com>
2021-12-23 09:24:13 -0800

    fixed pycodestyle for 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys, hidden_4
+import hidden_4
 if __name__ == "__main__":
     for item in dir(hidden_4):
-        if (item[:2]!= "__"):
+        if (item[:2] != "__"):
             print(item)
