2f45457efe86c9ff0e61fa15bc89e1e14bf03626
ojo-perezoso <bymartin24@gmail.com>
2021-12-23 07:29:58 -0800

    only executable as __main__ 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-res = 0
-if (len(sys.argv) > 1):
-    for i in range(1, len(sys.argv)):
-        res += int(sys.argv[i])
+if __name__ == "__main__":
+    res = 0
+    if (len(sys.argv) > 1):
+        for i in range(1, len(sys.argv)):
+            res += int(sys.argv[i])
 
-print(res)
+    print(res)
