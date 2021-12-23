978c7eb641dd409c5264e333435c57b721427ae2
ojo-perezoso <bymartin24@gmail.com>
2021-12-23 06:24:10 -0800

    Added 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+argc = (len(sys.argv))
+
+if (argc == 1):
+    print("0 arguments.")
+    exit()
+
+if (argc == 2):
+    print("1 argument:")
+else:
+    print("{:d} arguments:".format(argc - 1))
+
+for i in range(1, argc):
+    print("{:d}: {}".format(i, sys.argv[i]))
