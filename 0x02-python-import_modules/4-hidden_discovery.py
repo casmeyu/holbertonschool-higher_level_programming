7122aa61a4e4ac9c84f7fc71b03e6e1d5d070a1b
ojo-perezoso <bymartin24@gmail.com>
2021-12-23 09:15:57 -0800

    fix try 1 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys, hidden_4
-if __name__ = "__main__":
+if __name__ == "__main__":
     for item in dir(hidden_4):
         if (item[0] != "_" or item[1] != "_"):
             print(item)
