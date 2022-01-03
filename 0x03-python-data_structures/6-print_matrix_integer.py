391295b4af1a6644e2f56cc412028e0d7529346b
ojo-perezoso <bymartin24@gmail.com>
2022-01-03 09:18:45 -0800

    final fix for the matrix sequel

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 1 and not matrix[0]:
+        print()
+        pass
+
     for line in matrix:
         for i in range(len(line)):
             if i == len(line) - 1:
