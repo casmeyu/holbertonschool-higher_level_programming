ade10af9d0217e9c9b451d76d9f769684bd8d10a
ojo-perezoso <bymartin24@gmail.com>
2022-01-03 08:57:34 -0800

    fix try 1 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,6 @@
 def no_c(my_string):
     count = 0
     for char in my_string:
-        print(char)
         if (char == 'c' or char == 'C'):
             my_string = my_string[:count] + my_string[count + 1:]
             continue
