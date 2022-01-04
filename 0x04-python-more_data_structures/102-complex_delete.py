d26a3ad87cca6d269fb3aee8e9f5a3c31b7bd585
ojo-perezoso <bymartin24@gmail.com>
2022-01-04 15:09:58 -0800

    final fix 102-complex_delete.py tyGS

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    br = True
-    if value:
-        while br:
-            for key in a_dictionary:
-                if a_dictionary[key] == value:
-                    a_dictionary.pop(key)
-                    break
-            br = False
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
+            a_dictionary.pop(key)
 
     return (a_dictionary)
