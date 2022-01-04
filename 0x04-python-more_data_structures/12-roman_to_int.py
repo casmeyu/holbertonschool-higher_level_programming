76d8a2bb708507df8d481357b1150469ca8494f1
ojo-perezoso <bymartin24@gmail.com>
2022-01-04 10:48:29 -0800

    Added 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romans = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    res = 0
+    idx = 1
+    if (not roman_string):
+        return (res)
+
+    for idx in range(1, len(roman_string)):
+        if romans[roman_string[idx - 1]] < romans[roman_string[idx]]:
+            res += (romans[roman_string[idx - 1]] * -1)
+        else:
+            res += romans[roman_string[idx - 1]]
+
+    res += romans[roman_string[idx - 1]]
+
+    return res
