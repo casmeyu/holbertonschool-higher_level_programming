e886095a6d1ea7c4b4e028c2090202dbf66a02dd
ojo-perezoso <bymartin24@gmail.com>
2022-01-04 07:20:31 -0800

    Added 4-onlt_diff_elements.py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):    
+    diff_list = []
+    for item in set_1:
+        if item not in set_2:
+            diff_list.append(item)
+
+    for item in set_2:
+        if item not in set_1:
+            diff_list.append(item)
+
+    return (diff_list)
