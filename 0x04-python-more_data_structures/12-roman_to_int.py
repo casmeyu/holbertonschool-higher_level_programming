be7ccd37d17a1f5a1987aeeaa268bf487d503431
ojo-perezoso <bymartin24@gmail.com>
2022-01-04 10:57:37 -0800

    dont know what to fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,7 +13,8 @@ def roman_to_int(roman_string):
     idx = 1
     if (not roman_string or type(roman_string) != str):
         return (0)
-
+    if (len(roman_string) == 1):
+        return (romans[roman_string[0]])
     for idx in range(1, len(roman_string)):
         if romans[roman_string[idx - 1]] < romans[roman_string[idx]]:
             res += (romans[roman_string[idx - 1]] * -1)
