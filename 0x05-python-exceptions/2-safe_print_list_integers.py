ea7c33dec0d4cd2bfc37452408bdb4abc447800a
ojo-perezoso <bymartin24@gmail.com>
2022-01-17 07:15:30 -0800

    Removed IndexError Exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,11 +6,10 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             count += 1
-        except Exception as ex:
-            errname = type(ex).__name__
-            if (errname == "ValueError" or errname == "TypeError"):
-                continue
-            else:
-                break
+        except (ValueError, TypeError):
+            # errname = type(ex).__name__
+            # if (errname == "ValueError" or errname == "TypeError"):
+            continue
+
     print()
     return(count)
