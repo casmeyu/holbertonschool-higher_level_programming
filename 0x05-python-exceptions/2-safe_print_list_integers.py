f45ae84ad0cf5ae10fd99af74f6277f961d565f0
ojo-perezoso <bymartin24@gmail.com>
2022-01-17 06:10:44 -0800

    fix typeerror handling 2-safe_print_list_integer.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end='')
             count += 1
         except Exception as ex:
-            if (type(ex).__name__ == "ValueError"):
+            errname = type(ex).__name__
+            if (errname == "ValueError" or errname == "TypeError"):
                 continue
             else:
                 break
