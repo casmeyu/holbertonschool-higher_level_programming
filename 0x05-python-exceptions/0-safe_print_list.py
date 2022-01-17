dd8210c247ce3726dd097b02a922e6edac1f33c4
ojo-perezoso <bymartin24@gmail.com>
2022-01-17 05:18:31 -0800

    Added 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            counter += 1
+        except (IndexError, TypeError):
+            break
+    print()
+    return (counter)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,3 @@
+#0x05 python exceptions
+first exercices about error handling
+usage of try ... catch
