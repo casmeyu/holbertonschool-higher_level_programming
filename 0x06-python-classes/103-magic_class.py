55d48ebc644447c592a3873ceb86571c4bc60aed
ojo-perezoso <bymartin24@gmail.com>
2022-01-18 18:45:53 -0800

    dont know

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from dis import dis
 """Reverse engenieered from Python Bytecode"""
 
 
@@ -7,16 +8,19 @@ class MagicClass:
     def __init__(self, radius=0):
         """Creation of the circle"""
         self.__radius = 0
-        if type(radius) is not int:
-            if type(radius) is not float:
-                raise TypeError("radius must be a number")
+        if (type(radius) is not int):
+                if (type(radius) is not float):
+                    raise TypeError("radius must be a number")
+        
         self.__radius = radius
-        return None
+        
 
     def area(self):
         """Area of a circle"""
-        return (self.___radius ** 2) * math.pi
+        return((self.___radius ** 2) * math.pi)
 
-    def circumference(self):
+    def circumference(self, radius):
         """Circumference of the circle"""
         return (2 * math.pi) * self.__radius
+
+print(dis(MagicClass))
