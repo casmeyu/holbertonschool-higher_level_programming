a498f0b0f1ad3cb807ea5b8724d0d9e3a98426cc
ojo-perezoso <bymartin24@gmail.com>
2022-01-18 18:26:39 -0800

    Added 103-magic_class.py its a magic circle

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Reverse engenieered from Python Bytecode"""
+
+
+class MagicClass:
+    """Class: Its a magic circle"""
+    def __init__(self, radius=0):
+        """Creation of the circle"""
+        self.__radius = 0
+        if type(radius) is not int:
+            if type(radius) is not float:
+                raise TypeError("radius must be a number")
+        self.__radius = radius
+        return None
+
+    def area(self):
+        """Area of a circle"""
+        return (self.___radius ** 2) * math.pi
+
+    def circumference(self):
+        """Circumference of the circle"""
+        return (2 * math.pi) * self.__radius
