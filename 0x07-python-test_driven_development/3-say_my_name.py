90c31bcbd1f78f467ec6167c485ed91284acba18
ojo-perezoso <bymartin24@gmail.com>
2022-01-21 15:50:06 -0800

    Added 3-say_my_name and some tests for printint first and last name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""say may name
+prints <first_name> <last_name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    msg = ""
+    if (type(first_name) is not str):
+        raise TypeError("first_name must be a string")
+
+    if (type(last_name) is not str):
+        raise TypeError("last_name must be a string")
+
+    msg += first_name
+    if (len(last_name) > 0):
+        msg += " {}".format(last_name)
+
+    print(msg)
