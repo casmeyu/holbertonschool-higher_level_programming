2c5c839b7804461a508ce2d1fce1b562bdde7f93
ojo-perezoso <bymartin24@gmail.com>
2022-01-24 13:44:17 -0800

    fix try 1 __str__

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -65,10 +65,12 @@ class Rectangle():
     def __str__(self):
         res = ""
         if (self.height == 0):
-            print()
+            pass
         else:
-            for c_idx in range(self.height):
+            for c_idx in range(self.height - 1):
                 res += ("#"*self.width + "\n")
+            res += ("#"*self.width)
+
         return(res)
 
     def __repr__(self):
