0717e6346cce8cf906e59500557242723429f079
ojo-perezoso <bymartin24@gmail.com>
2022-01-24 14:03:59 -0800

    fixed missing blank space at __repr__

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -67,5 +67,5 @@ class Rectangle():
         return(res)
 
     def __repr__(self):
-        res = "{}({},{})".format(type(self).__name__, self.width, self.height)
+        res = "{}({}, {})".format(type(self).__name__, self.width, self.height)
         return(res)
