7f8e61718ddf6c953d5d110d2832e9deae24b740
ojo-perezoso <bymartin24@gmail.com>
2022-01-24 14:01:52 -0800

    added if self.width == 0

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -57,7 +57,7 @@ class Rectangle():
 
     def __str__(self):
         res = ""
-        if (self.height == 0):
+        if (self.height == 0 or self.width == 0):
             return(res)
         else:
             for c_idx in range(self.height - 1):
