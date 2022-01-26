be6686446b6a12705ffd9cece851c6f9e1c1c5b8
ojo-perezoso <bymartin24@gmail.com>
2022-01-26 15:14:29 -0800

    Added documentation for lazy matrix

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 from numpy import matmul
-
+"""Lazy matrix multiplication using numpy library"""
 
 def lazy_matrix_mul(m_a, m_b):
+    """lazy multiplication
+    Arguments:
+        m_a ([[]]): first matrix
+        m_b ([[]]): second matrix
+    """
     return(matmul(m_a, m_b))
