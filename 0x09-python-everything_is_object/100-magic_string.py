eef3b58a6e7b0369ef083a413d26d3ddfc67366e
ojo-perezoso <bymartin24@gmail.com>
2022-01-25 17:58:51 -0800

    fix magic_string with parameter

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    # magic_string.i if hasattr(magic_string, "i") else magic_string.i = 1
-    res = ""
-    if hasattr(magic_string, "i"):
-        magic_string.i += 1
-    else:
-        magic_string.i = 1
-
-    for x in range(magic_string.i):
-        res += "BestSchool"
-        if x != (magic_string.i - 1):
-            res += ", "
-
-    return(res)
+def magic_string(val=[]):
+    val.append("BestSchool")
+    return(", ".join(val))
