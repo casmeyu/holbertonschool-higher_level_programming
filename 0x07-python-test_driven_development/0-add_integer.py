c0efe9d1638fdc0dc1ad91a6114973673ab8201d
ojo-perezoso <bymartin24@gmail.com>
2022-01-26 14:52:02 -0800

    Added float(inf) and float(NaN) Test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,6 +2,8 @@
 """
 Add integer for testing
 """
+
+
 def add_integer(a, b=98):
     """adds a and b
     Attributes:
@@ -13,7 +15,7 @@ def add_integer(a, b=98):
             raise TypeError("a must be an integer")
         else:
             a = int(a)
-    
+
     if (type(b) is not int):
         if (type(b) is not float):
             raise TypeError("b must be an integer")
