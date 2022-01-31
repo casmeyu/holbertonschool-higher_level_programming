dee1f585f304a3359db5cc27b4b4ca4c864bfbb3
ojo-perezoso <bymartin24@gmail.com>
2022-01-31 05:20:24 -0800

    Added 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usb/bin/python3
+"""Lookupmodule
+prints information about objects
+"""
+
+
+def lookup(object):
+    return list(dir(object))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+# 0x0A python inheritance
+First exercices about object inheritance with python
