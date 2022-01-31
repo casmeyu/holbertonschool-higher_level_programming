9f313cf7907d4d931bca51dab230153f26989615
ojo-perezoso <bymartin24@gmail.com>
2022-01-31 09:51:53 -0800

    fix try 4-inherited_from_class.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """Initializes the square with a positive size""" 
+        Rectangle.integer_validator(self, 'size', size)
+        super().__init__(size, size)
+        self.__size = size
+    
+    def area(self):
+        """Returns the area of the square"""
+        return (self.__size ** 2)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,6 @@ def inherits_from(obj, a_class):
             obj: object to check
             a_class: class to check against
     """
+    if (type(obj) == a_class):
+        return False
     return (issubclass(type(obj), a_class))
