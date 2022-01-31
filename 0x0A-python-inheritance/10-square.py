c03c06d1b7f7d12a9980d66e3220d450d861a547
ojo-perezoso <bymartin24@gmail.com>
2022-01-31 09:58:06 -0800

    fix coding style for 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,13 +2,14 @@
 """Module for square"""
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     def __init__(self, size):
-        """Initializes the square with a positive size""" 
+        """Initializes the square with a positive size"""
         Rectangle.integer_validator(self, 'size', size)
         super().__init__(size, size)
         self.__size = size
-    
+
     def area(self):
         """Returns the area of the square"""
         return (self.__size ** 2)
