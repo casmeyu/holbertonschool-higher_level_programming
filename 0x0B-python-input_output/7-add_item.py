560b4e0121064eeea46a073965eaf317248dfeaa
ojo-perezoso <bymartin24@gmail.com>
2022-02-01 12:13:04 -0800

    final fix for 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,12 +2,14 @@
 """This Script adds all its arguments to a list
     and saves them into a file
 """
-import sys
+from sys import argv
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = [arg for arg in sys.argv]
-
-save_to_json_file(args[1:], 'add_item.json')
+try:
+    old_add = load_from_json_file('add_item.json')
+    save_to_json_file(old_add + argv[1:], 'add_item.json')
+except Exception as ex:
+    save_to_json_file(argv[1:], 'add_item.json')
