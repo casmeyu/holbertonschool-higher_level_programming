4186e0e96fef82d26e2f214ad3a6c8d0cd4caaa5
ojo-perezoso <bymartin24@gmail.com>
2022-02-01 05:25:25 -0800

    Added 6-load_from_json.py

---
+++ 0x0B-python_input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Moduel for reading a json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """Reads objects from a json file
+        Arguments:
+            filename (str): path of the file to read
+    """
+    with open(filename) as f:
+        for line in f:
+            return (json.loads(line))
