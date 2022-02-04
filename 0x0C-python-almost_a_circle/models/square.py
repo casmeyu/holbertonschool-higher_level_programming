f029f8ff982ef6ad8445b08a388ff800f9e9b230
ojo-perezoso <bymartin24@gmail.com>
2022-02-04 10:40:44 -0800

    Added size getter and setter for square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,11 +12,27 @@ class Square(Rectangle):
             __x (int): x position of the square
             __y (int): y position of the square
     """
-    def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
+    @property
+    def size(self):
+        return (self.width)
+
+    @size.setter
+    def size(self, size):
+        self.width = size
+        self.height = size
 
     # Built in functions
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialization of Square
+            Arguemnts:
+                size (int): must be >= 0
+                x (int): must be > 0
+                y (int): msut be > 0
+                id (int): must be > 0
+        """
+        super().__init__(size, size, x, y, id)
+
     def __str__(self):
         txt = f"[Square] ({self.id}) {self.x}/{self.y}"
-        txt += f"{self.height}/{self.width}"
+        txt += f"{self.height}/{self.width} - {self.width}"
         return txt
