f2feb94ba89f0397338ddf515b43a95a6a985b28
ojo-perezoso <bymartin24@gmail.com>
2022-02-04 11:02:10 -0800

    Added update functionality for Square Class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,6 +21,18 @@ class Square(Rectangle):
         self.width = size
         self.height = size
 
+    def update(self, *args, **kwargs):
+        """Update Square function"""
+        atr_list = ['id', 'size', 'x', 'y']
+        if (args and (len(args) < 5 and len(args) > 0)):
+            for idx in range(len(args)):
+                setattr(self, atr_list[idx], args[idx])
+            return
+
+        for key in kwargs:
+            if key in atr_list:
+                setattr(self, key, kwargs[key])
+
     # Built in functions
     def __init__(self, size, x=0, y=0, id=None):
         """Initialization of Square
@@ -33,6 +45,5 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        txt = f"[Square] ({self.id}) {self.x}/{self.y}"
-        txt += f"{self.height}/{self.width} - {self.width}"
+        txt = f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
         return txt
