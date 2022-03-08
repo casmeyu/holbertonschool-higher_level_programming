c2de9215c4fe1ece609c7b3f27dba655f9c46867
ojo-perezoso <bymartin24@gmail.com>
2022-03-08 07:35:05 -0800

    add: count in groups

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- 14. average score
 -- Computes the average score of all the playrs in second_table
-SELECT AVG(score) as average from second_table;
+SELECT AVG(score) AS average FROM second_table;
