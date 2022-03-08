5f0bf1768cb3ba6063979590ba6cc157c1eb2c5a
ojo-perezoso <bymartin24@gmail.com>
2022-03-08 09:09:34 -0800

    fix: now database and table are also in utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,5 @@
 -- Lets go to utf8
 -- Converts database to utf8mb4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8;
