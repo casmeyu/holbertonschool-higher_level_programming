45ef01cf856973ddd9711bb4fb51da2767c6b168
ojo-perezoso <bymartin24@gmail.com>
2022-03-09 08:59:11 -0800

    fix: now showing NULL genres thanks to LEFT JOIN

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@
 SELECT s.title, g.name FROM tv_shows AS s
 LEFT JOIN tv_show_genres AS sg
 ON sg.show_id = s.id
-INNER JOIN tv_genres AS g
+LEFT JOIN tv_genres AS g
 ON g.id = sg.genre_id
 ORDER BY s.title, g.name ASC;
