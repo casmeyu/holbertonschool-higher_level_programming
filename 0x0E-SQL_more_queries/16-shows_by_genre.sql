7e96b78d898ca030f1d89f4b35a9c344ed6fa2bf
ojo-perezoso <bymartin24@gmail.com>
2022-03-09 08:57:45 -0800

    add: all shows and their genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- 16. shows by genre
+-- Lists all the shows and their genres
+SELECT s.title, g.name FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS sg
+ON sg.show_id = s.id
+INNER JOIN tv_genres AS g
+ON g.id = sg.genre_id
+ORDER BY s.title, g.name ASC;
