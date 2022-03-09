9504c4d1face35d434b62199e2cf3d94e877a119
ojo-perezoso <bymartin24@gmail.com>
2022-03-09 07:16:24 -0800

    fix: order by city.id asc

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,4 @@
 -- Selects all the cities from California with a subquery
 SELECT * FROM cities
 WHERE (SELECT id FROM states WHERE states.name = "California") = cities.state_id
-ORDER BY cities.id; 
+ORDER BY cities.id ASC;
