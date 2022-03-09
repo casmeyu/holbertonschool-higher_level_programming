0478c3e5db9877cd732608c5d26d20c75bf12abf
ojo-perezoso <bymartin24@gmail.com>
2022-03-09 08:19:48 -0800

    add: count shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- 13. shows by genre count
+-- Counts the ammount of shows in each genre
+SELECT g.name, COUNT(*) AS number_of_shows FROM tv_genres AS g
+INNER JOIN tv_show_genres AS s
+ON s.genre_id = g.id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
