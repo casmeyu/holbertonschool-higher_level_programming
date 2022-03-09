81c7bb657ff5496c74c2b87ba5b96d0d4738ea28
ojo-perezoso <bymartin24@gmail.com>
2022-03-09 07:20:51 -0800

    fix: output id - name

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- 8. cities of california
 -- Selects all the cities from California with a subquery
-SELECT * FROM cities
+SELECT cities.id, cities.name FROM cities
 WHERE (SELECT id FROM states WHERE states.name = "California") = cities.state_id
 ORDER BY cities.id ASC;
