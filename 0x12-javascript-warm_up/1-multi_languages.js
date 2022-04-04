dbbdfcb35bf12fb0112db45c9b02d5783c026cc2
ojo-perezoso <bymartin24@gmail.com>
2022-04-04 08:02:28 -0700

    add first exercices

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+const langs = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(langs);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,3 @@
+# 0x12 Javascript warm up
+First exercices with js
+Complacent with the semistandard standard
