89b2c31405001f81df810f929624af0e13e97fd2
ojo-perezoso <bymartin24@gmail.com>
2022-04-04 13:15:47 -0700

    fix: fixed for no arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 const numbers = [];
 
-for (let i = 2; process.argv[i]; i++) {
-  numbers.push(parseInt(process.argv[i]));
+if (process.argv.length < 4) {
+  console.log(0);
+} else {
+  for (let i = 2; process.argv[i]; i++) {
+    numbers.push(parseInt(process.argv[i]));
+  }
+  numbers.sort((a, b) => { return (b - a); });
+  console.log(numbers[1]);
 }
-numbers.sort((a, b) => { return (b - a); });
-console.log(numbers[1]);
