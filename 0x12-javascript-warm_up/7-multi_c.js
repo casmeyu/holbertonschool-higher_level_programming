1a3a8b5ca0db8cd57062dddda76d04b74506bd6d
ojo-perezoso <bymartin24@gmail.com>
2022-04-04 10:27:11 -0700

    fix: check for a negative number of occurrences

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 let num = parseInt(process.argv[2]);
 if (isNaN(num)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (num > 0){
   while (num !== 0) {
     console.log('C is fun');
     num--;
