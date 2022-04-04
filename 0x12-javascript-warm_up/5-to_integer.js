d4f05328c1532a632093ac39bc9224b42191fd44
ojo-perezoso <bymartin24@gmail.com>
2022-04-04 09:59:59 -0700

    fix: saved parsed number on a const

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2]))) {
+const num = parseInt(process.argv[2]);
+if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log('My number is: ' + process.argv[2]);
+  console.log('My number: ' + num);
 }
