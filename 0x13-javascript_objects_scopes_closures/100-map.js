31b6507d7d9f0e3de7eb5c3db9a04ac97bc9a017
ojo-perezoso <bymartin24@gmail.com>
2022-04-05 12:39:57 -0700

    fix: multiplication of item

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,5 +2,5 @@
 const data = require('./100-data').list;
 
 console.log(data);
-const new_data = data.map((item) => item * 2);
+const new_data = data.map((item, idx) => item * idx);
 console.log(new_data);
