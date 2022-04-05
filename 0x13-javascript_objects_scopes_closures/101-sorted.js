eaf27d1a905a7c24833531891eedb2d33b132d05
ojo-perezoso <bymartin24@gmail.com>
2022-04-05 12:09:54 -0700

    fix: coding style fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
-const new_dict = {};
+const newDict = {};
 
 console.log(dict);
 for (const [key, value] of Object.entries(dict)) {
-  if (new_dict[value]) {
-    new_dict[value].push(key);
+  if (newDict[value]) {
+    newDict[value].push(key);
   } else {
-    new_dict[value] = [key];
+    newDict[value] = [key];
   }
 }
-console.log(new_dict);
+console.log(newDict);
