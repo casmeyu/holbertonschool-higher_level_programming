ba300cec93f80bacc546f360039fe98f52c1a367
ojo-perezoso <bymartin24@gmail.com>
2022-04-05 09:43:45 -0700

    add: print a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  checkNumber (n) {
+    return (!isNaN(parseInt(n)));
+  }
+
+  constructor (w, h) {
+    if (this.checkNumber(w) && this.checkNumber(h)) {
+      w = parseInt(w);
+      h = parseInt(h);
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  }
+
+  print () {
+    const row = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+};
