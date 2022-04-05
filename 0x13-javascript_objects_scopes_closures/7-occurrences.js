22616f72227eb6ca653e299e05cb78288c81889f
ojo-perezoso <bymartin24@gmail.com>
2022-04-05 10:52:02 -0700

    add: number of occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let cont = 0;
-  for (let idx = 0; list[idx]; idx++) {
-    if (list[idx] == searchElement) {
+  for (let idx = 0; idx < list.length; idx++) {
+    if (list[idx] === searchElement) {
       cont++;
     }
   }
   return (cont);
-}
+};
