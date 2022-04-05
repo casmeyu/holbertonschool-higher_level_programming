9a220ff0e101b9fc2b720fbd077952150c212750
ojo-perezoso <bymartin24@gmail.com>
2022-04-05 12:11:49 -0700

    fix: removed unnecesary print

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,7 +2,6 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 
-console.log(dict);
 for (const [key, value] of Object.entries(dict)) {
   if (newDict[value]) {
     newDict[value].push(key);
