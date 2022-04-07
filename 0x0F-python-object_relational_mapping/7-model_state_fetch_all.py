1d5c062e8b50cdf89ae4ebdcd608b622d897438c
ojo-perezoso <bymartin24@gmail.com>
2022-04-07 12:36:02 -0700

    add: first query with a session and sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module for printing all the states"""
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == '__main__':
+    eng_creation = f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}'
+    engine = create_engine(eng_creation)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print(f'{instance.id}: {instance.name}')
