269cbc8411d488da4d176a9b01aac7c8dff53672
ojo-perezoso <bymartin24@gmail.com>
2022-04-07 11:21:55 -0700

    add: add a join query

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import MySQLdb
+from sys import argv
+
+try:
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cur = db.cursor()
+except Exception as ex:
+    print(ex)
+
+try:
+    query = f'''select c.id, c.name, s.name from cities as c
+            left join states as s
+            on s.id = c.state_id'''
+
+    cur.execute(query)
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+except Exception as ex:
+    print(ex)
