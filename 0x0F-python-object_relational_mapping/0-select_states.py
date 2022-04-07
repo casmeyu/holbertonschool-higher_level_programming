40146a5f20293b895ff00d9e4400aaa1afc08498
ojo-perezoso <bymartin24@gmail.com>
2022-04-07 10:33:36 -0700

    add: first MySQLdb connection

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import MySQLdb
+from sys import argv
+
+try:
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cur = db.cursor()
+    print('did the connection')
+except Exception as ex:
+    print(ex)
+
+try:
+    query = '''select * from states;'''
+
+    cur.execute(query)
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+except Exception as ex:
+    print(ex)
