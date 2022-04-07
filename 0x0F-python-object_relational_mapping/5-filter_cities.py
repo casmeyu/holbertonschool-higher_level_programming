9c9c8c84726207d2e207c035d99083c1c2d69107
ojo-perezoso <bymartin24@gmail.com>
2022-04-07 11:35:53 -0700

    add: create filter cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import MySQLdb
+from sys import argv
+
+try:
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cur = db.cursor()
+except Exception as ex:
+    print(ex)
+
+try:
+    query = f'''select c.name from states as s
+            left join cities as c
+            on c.state_id = s.id
+            where s.name = %s'''
+
+    cur.execute(query, (argv[4],))
+    query_rows = cur.fetchall()
+
+    res = ''
+    for idx in range(len(query_rows) - 1):
+        row = query_rows[idx]
+        row = str(row).strip("('")
+        row = row.strip("',)")
+        if idx != 0:
+            res += (', ' + row)
+        else:
+            res += (row)
+    print(res)
+except Exception as ex:
+    print(ex)
