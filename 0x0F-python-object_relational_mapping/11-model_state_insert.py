34fcae0e057b9f71351a0a6f0731d3e3ea1e57a6
ojo-perezoso <bymartin24@gmail.com>
2022-04-08 07:38:34 -0700

    add: insert with orm sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module for printing all the states"""
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from sys import argv
+
+
+if __name__ == '__main__':
+    eng_creation = f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}'
+    engine = create_engine(eng_creation)
+    session = Session(engine)
+
+    new_state = State(nam='Arizona')
+
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+    session.close()
