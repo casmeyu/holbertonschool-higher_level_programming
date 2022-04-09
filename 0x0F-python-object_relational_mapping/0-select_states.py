6c2ac49d1b5010194387ebefc078d87b57229cdd
ojo-perezoso <bymartin24@gmail.com>
2022-04-09 08:00:31 -0700

    add: module docstring

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,28 +3,30 @@
 import MySQLdb
 from sys import argv
 
-try:
-    db = MySQLdb.connect(
+
+if __name__ == '__main__':
+    try:
+        db = MySQLdb.connect(
             host='localhost',
             port=3306,
             user=argv[1],
             passwd=argv[2],
             db=argv[3]
             )
-    cur = db.cursor()
-except Exception as ex:
-    print(ex)
+        cur = db.cursor()
+    except Exception as ex:
+        print(ex)
 
-try:
-    query = '''select * from states;'''
+    try:
+        query = '''select * from states;'''
 
-    cur.execute(query)
-    query_rows = cur.fetchall()
+        cur.execute(query)
+        query_rows = cur.fetchall()
 
-    for row in query_rows:
-        print(row)
+        for row in query_rows:
+            print(row)
 
-    cur.close()
-    db.close()
-except Exception as ex:
-    print(ex)
+        cur.close()
+        db.close()
+    except Exception as ex:
+        print(ex)
