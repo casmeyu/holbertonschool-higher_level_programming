ef8415b3c38fbee04bbd8e96698765bef274411a
ojo-perezoso <bymartin24@gmail.com>
2022-04-09 13:59:45 -0700

    fix try: removed exceptions

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,29 +5,24 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    try:
-        db = MySQLdb.connect(
-            host='localhost',
-            port=3306,
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3]
-            )
-        cur = db.cursor()
-    except Exception as ex:
-        print(ex)
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+        )
+    cur = db.cursor()
 
-    try:
-        query = f'''select * from states
-                where name like "{argv[4]}"'''
+    query = f'''select * from states
+        where name like binary "{argv[4]}"
+        order by states.id asc'''
 
-        cur.execute(query)
-        query_rows = cur.fetchall()
+    cur.execute(query)
+    query_rows = cur.fetchall()
 
-        for row in query_rows:
-            print(row)
+    for row in query_rows:
+        print(row)
 
-        cur.close()
-        db.close()
-    except Exception as ex:
-        print(ex)
+    cur.close()
+    db.close()
