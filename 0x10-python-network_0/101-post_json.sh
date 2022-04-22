16ba1ed000354f473db532018a347ca536f435b5
ojo-perezoso <bymartin24@gmail.com>
2022-04-22 20:38:40 +0000

    fix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a Post request with the contents of a json file
-curl -X POST -H 'Content-Type: application/json' -d @$2 $1
+curl -s -X POST -H 'Content-Type: application/json' -d @$2 $1
