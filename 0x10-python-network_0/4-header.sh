e5f9345bd5d166399117c8d762e376d67a4df339
ojo-perezoso <bymartin24@gmail.com>
2022-04-22 18:54:42 +0000

    add: find first peak value

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # HTTP request with HEADER arguments (key: value)
-curl -H "X-HolbertonSchool-User-Id: 98" -s $1
+curl -H "X-School-User-Id: 98" -s $1
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,3 +3,20 @@
 
 def find_peak(list_of_integers):
     """ Returns the peak of a list """
+    l = list_of_integers
+    size = len(l)
+    if size == 0:
+        return
+    if size == 1:
+        return l[0]
+
+    for idx in range(size - 1):
+        if idx == 0 and idx + 1 < size:
+            if l[idx] >= l[idx + 1]:
+                return l[idx]
+        elif idx + 1 < size:
+            if l[idx] >= l[idx - 1] and l[idx] >= l[idx + 1]:
+                return l[idx]
+        else:
+            if l[idx] >= l[idx - 1]:
+                return l[idx]
