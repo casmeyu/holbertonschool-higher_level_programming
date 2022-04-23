c626945b9a77628f7bab26e2f00fde48cf3323b6
ojo-perezoso <bymartin24@gmail.com>
2022-04-23 16:04:03 +0000

    using github api

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Uses the github api to get last 10 commits from a given repo
+import json
+from urllib import request
+from sys import argv
+
+
+max_commits = 10
+req = 'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2])
+req = request.Request(req)
+req.add_header('accept', 'application/vnd.github.v3+json')
+
+with request.urlopen(req) as res:
+    data = res.read().decode('utf-8')
+    data = json.loads(data)
+    cont = 0
+    for commit in data:
+        m = "{}: {}".format(commit['sha'], commit['commit']['author']['name'])
+        print(m)
+        cont += 1
+        if cont >= max_commits:
+            break
