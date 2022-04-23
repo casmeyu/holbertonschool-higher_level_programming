58a079cd80bfd284d55b606e2bc7a5165c02516d
ojo-perezoso <bymartin24@gmail.com>
2022-04-23 15:15:04 +0000

    trouble with the tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,10 @@ if __name__ == '__main__':
     with request.urlopen('https://intranet.hbtn.io/status') as res:
         data = res.read()
         message = "Body response:\n\
-\t- type: {}\n\
-\t- content: {}".format(type(data), data)
+    - type: {}\n\
+    - content: {}".format(type(data), data)
         # message = f"Body response: type: {type(data)} - content: {data}"
         # why in the hell is the f"String" throws a Syntax Error!?!?
         if res.headers.get_content_charset() == 'utf-8':
-            message += '\n\t- utf8 content: OK'
+            message += '\n    - utf8 content: OK'
         print(message)
