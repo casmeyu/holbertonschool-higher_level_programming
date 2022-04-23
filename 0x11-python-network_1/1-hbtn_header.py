5accb582f0f4e80598288e10e4e9150068533575
ojo-perezoso <bymartin24@gmail.com>
2022-04-23 14:32:06 +0000

    fix: now url is an argv

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This scripts returns the value of a specific Header value of http request"""
 from urllib import request
+from sys import argv
 
 
 if __name__ == '__main__':
-    with request.urlopen('https://intranet.hbtn.io') as res:
-        search = 'X-Request-Id'
-        print(res.headers.get(search))
+    if len(argv) > 1:
+        with request.urlopen(argv[1]) as res:
+            search = 'X-Request-Id'
+            print(res.headers.get(search))
