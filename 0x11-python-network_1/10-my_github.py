8aeba7ee0546aed38ee7ec69a060ea6076937ae0
Martin Casamayou <42519492+ojo-perezoso@users.noreply.github.com>
2022-04-24 18:38:53 -0300

    Update 10-my_github.py
    Change link from users to user....

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,7 +5,7 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/users/{}'.format(argv[1])
+    url = 'https://api.github.com/user/{}'.format(argv[1])
     res = requests.get(url, auth=(argv[1], argv[2]))
     try:
         data = res.json()
