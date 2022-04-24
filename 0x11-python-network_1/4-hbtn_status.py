0f13dedd5890c3961b76790d61127b3d86a4b492
ojo-perezoso <bymartin24@gmail.com>
2022-04-24 13:46:41 +0000

    fix: now just using requests module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """This Script sends an http request with urllib and prints some information"""
-from urllib import request
+import requests
 
 
 if __name__ == '__main__':
-    with request.urlopen('https://intranet.hbtn.io/status') as res:
-        data = res.read().decode('utf-8')
-        message = "Body response:\n\
+    res = requests.get('https://intranet.hbtn.io/status')
+    message = "Body response:\n\
 \t- type: {}\n\
-\t- content: {}".format(type(data), data)
-        print(message)
+\t- content: {}".format(type(res.text), res.text)
+    print(message)
