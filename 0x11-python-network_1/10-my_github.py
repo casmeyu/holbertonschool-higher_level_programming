ea0e921d3251665e920f7d8b28be2bb8117a1aed
ojo-perezoso <bymartin24@gmail.com>
2022-04-24 14:33:29 +0000

    fix: add not a valid JSON check

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,11 +7,11 @@ from sys import argv
 if __name__ == '__main__':
     url = 'https://api.github.com/users/{}'.format(argv[1])
     res = requests.get(url, auth=(argv[1], argv[2]))
-    try:
+    if res.headers.get('content-type') == 'application/json':
         data = res.json()
         try:
             print(data['id'])
         except KeyError as ex:
             print('None')
-    except Exception as ex:
+    else:
         print('Not a valid JSON')
