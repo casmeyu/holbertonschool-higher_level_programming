287559c03a36d7da69b949917287218b324272c0
ojo-perezoso <bymartin24@gmail.com>
2022-04-24 22:06:09 +0000

    changed argv order

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,9 +6,8 @@ from sys import argv
 
 max_commits = 10
 cont = 0
-url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2])
-header = {'accept': 'application/vnd.github.v3+json'}
-res = requests.get(url, headers=header)
+url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+res = requests.get(url)
 data = res.json()
 for c in data:
     msg = "{}: {}".format(c['sha'], c['commit']['author']['name'])
