eed1f8a1d1808012517b4f9162de27fe301bdb32
ojo-perezoso <bymartin24@gmail.com>
2022-05-17 20:20:59 +0000

    add: how many times does this character appears

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/* Returns the name of a starwars film given a movie number */
+const axios = require('axios').default;
+const process = require('process');
+
+if (process.argv.length >= 3) {
+  const url = process.argv[2];
+  const search = 'https://swapi-api.hbtn.io/api/people/18/';
+  axios.get(url)
+    .then((response) => {
+      let count = 0;
+      response.data.results.forEach((film) => {
+        film.characters.forEach((character) => {
+          if (character == search) {
+            count += 1;
+          }
+        });
+      });
+      console.log(count);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
